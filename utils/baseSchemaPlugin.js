f975f4e878dc2a6cc21031931533a82fdd94b1f6
Makinde Adeagbo <makinde@devcolor.org>
2018-09-21 16:18:03 -0700

    Remove version keys from serialization of mongoose documents (#9)
    * Remove version keys from serialization of mongoose documents
    
    We never use the `__v` key, which keeps tack of how many times the document has been edited.
    
    * Better handling of duplicate key errors
    
    Basically, mongoose bubbles up native errors from mongodb when there's a duplicate key error. This plugin makes those errors into mongoose validation errors, which is nice.
    
    * Adding newer packages

--- utils/baseSchemaPlugin.js
+++ utils/baseSchemaPlugin.js
@@ -3,6 +3,7 @@ const http = require('http');
 const selectVirtuals = require('mongoose-select-virtuals');
 const leanVirtuals = require('mongoose-lean-virtuals');
 const idValidator = require('mongoose-id-validator');
+const beautifyUnique = require('mongoose-beautiful-unique-validation');
 // const authz = require('mongoose-authz');
 const shortId = require('shortid');
 
@@ -11,13 +12,14 @@ module.exports = (schema) => {
   schema.path('_id').default(shortId.generate);
 
   schema.set('timestamps', true);
-  schema.set('toObject', { getters: true, versionKey: true });
-  schema.set('toJSON', { getters: true, versionKey: true });
+  schema.set('toObject', { getters: true, versionKey: false });
+  schema.set('toJSON', { getters: true, versionKey: false });
   schema.set('strictQuery', true);
 
   schema.plugin(idValidator);
   schema.plugin(leanVirtuals);
   schema.plugin(selectVirtuals);
+  schema.plugin(beautifyUnique);
   // TODO (makinde 8/25/18): Bring back when ready to implement auth across the board
   // schema.plugin(authz);
 
