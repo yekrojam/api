c7789bf7263e7db7d2007edd3ef60c30003da3a4
Makinde Adeagbo <makinde@devcolor.org>
2018-09-27 01:05:11 -0700

    Adding tests for baseSchemaPlugin (#12)

--- utils/baseSchemaPlugin.js
+++ utils/baseSchemaPlugin.js
@@ -1,10 +1,7 @@
-const _ = require('lodash');
-const http = require('http');
 const selectVirtuals = require('mongoose-select-virtuals');
 const leanVirtuals = require('mongoose-lean-virtuals');
 const idValidator = require('mongoose-id-validator');
 const beautifyUnique = require('mongoose-beautiful-unique-validation');
-// const authz = require('mongoose-authz');
 const shortId = require('shortid');
 
 module.exports = (schema) => {
@@ -12,21 +9,38 @@ module.exports = (schema) => {
   schema.path('_id').default(shortId.generate);
 
   schema.set('timestamps', true);
-  schema.set('toObject', { getters: true, versionKey: false });
-  schema.set('toJSON', { getters: true, versionKey: false });
   schema.set('strictQuery', true);
+
+  const toObjectOptions = {
+    getters: true,
+    versionKey: false,
+    transform(doc, ret) {
+      const { _id, ...rest } = ret;
+      return rest;
+    },
+  };
+  schema.set('toObject', toObjectOptions);
+  schema.set('toJSON', toObjectOptions);
 
   schema.plugin(idValidator);
   schema.plugin(leanVirtuals);
   schema.plugin(selectVirtuals);
   schema.plugin(beautifyUnique);
-  // TODO (makinde 8/25/18): Bring back when ready to implement auth across the board
-  // schema.plugin(authz);
 
-  // H4x
-  schema.post('findOne', (doc) => {
-    if (_.isEmpty(doc)) {
-      throw new Error(http.STATUS_CODES[404]);
-    }
-  });
+  // We want to strip out `_id` and `__v` from lean qeries, just like we do with `toJSON` and
+  // `toObject`.
+  function stripDocumentArtifacts(res) {
+    if (!this._mongooseOptions.lean) return;
+
+    const docs = Array.isArray(res) ? res : [res];
+
+    docs.forEach((doc) => {
+      delete doc._id; // eslint-disable-line no-param-reassign
+      delete doc.__v; // eslint-disable-line no-param-reassign
+    });
+  }
+
+  schema.post('find', stripDocumentArtifacts);
+  schema.post('findOne', stripDocumentArtifacts);
+  schema.post('findOneAndUpdate', stripDocumentArtifacts);
 };
