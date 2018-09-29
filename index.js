64b2f987b72dfaa0fa2f99012af6b97ab3194ff0
Eric Giovanola <ericgio@gmail.com>
2018-09-28 23:31:19 -0700

    Fix error

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(
   jwt({
     credentialsRequired: process.env.NODE_ENV === 'production',
     secret: process.env.JWT_SECRET,
-  })
+  }),
   router,
   (req, res) => {
     res.status(404).json({ message: http.STATUS_CODES[404] });
