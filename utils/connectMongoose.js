a3c73e432ba8972fe847065004ed48607d367077
Makinde Adeagbo <makinde@devcolor.org>
2018-10-05 22:43:38 -0700

    Changing name of DB URL env variable (#30)
    This brings it into line with what mLab and other providers use by default. Use the same.

--- utils/connectMongoose.js
+++ utils/connectMongoose.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const debug = require('debug')('majorkey:apiServer:dbConnection');
 
-const { NODE_ENV, MONGO_URI } = process.env;
+const { NODE_ENV, MONGODB_URI } = process.env;
 const isProd = NODE_ENV === 'production';
 const isDev = NODE_ENV === 'development';
 const accessLiveData = isProd || isDev;
-const URIToUse = accessLiveData ? MONGO_URI : 'http://localhost:27017';
+const URIToUse = accessLiveData ? MONGODB_URI : 'http://localhost:27017';
 const connectOptions = {
   useNewUrlParser: true,
 };
