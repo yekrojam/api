9557a756e0876c6625d26622bf6b90d93c27dce7
Makinde Adeagbo <makinde@devcolor.org>
2018-10-08 16:53:35 -0400

    Using Joi to enforce schema on environment (#31)
    Using Joi to enforce schema on environment
    
    This gives us much richer ways of specifying the environment variables that we need (beyond dotenv-safe's binary required or not). Should reduce issues of something not being available and make it slightly easier to make sure your dev app has everything.

---
+++ utils/envVerification.js
@@ -0,0 +1,20 @@
+const Joi = require('joi');
+/**
+ * Verifies which environment variables are required, present and well formed
+ */
+module.exports = {
+  verify() {
+    const schema = {
+      MONGODB_URI: Joi.string().uri().required(),
+      NODE_ENV: Joi.valid('production', 'development', 'test').required(),
+      DEBUG: Joi.string(),
+    };
+
+    const result = Joi.validate(
+      process.env,
+      schema,
+      { abortEarly: false, allowUnknown: true },
+    );
+    if (result.error) throw result.error;
+  },
+};
