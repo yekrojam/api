ed9cba2548712ce432e1181f9189d2a31706e56a
Makinde Adeagbo <makinde@adeagbo.com>
2018-10-21 02:56:39 -0700

    Establishing a defaultedImageURL for users
    so clients can share this logic if they'd like. Since the field is a virtual, it can be selected just like other fields

--- userModel/__tests__/index.test.js
+++ userModel/__tests__/index.test.js
@@ -1,5 +1,6 @@
 const test = require('ava');
 const mongoose = require('mongoose');
+const axios = require('axios');
 
 const User = require('../');
 const referenceUserData = require('./referenceUserData');
@@ -21,11 +22,16 @@ test.serial('Validate that all fields are present after saving', async (t) => {
   t.is(refUser.email, referenceUserData.email, 'email should be unchanged');
   t.is(refUser.phone, referenceUserData.phone, 'phone should be unchanged');
   t.is(refUser.gender, referenceUserData.gender, 'gender should be unchanged');
-  t.is(refUser.birthYear, referenceUserData.birthYear, 'birthyear should be unchanged');
+  t.is(refUser.birthYear, referenceUserData.birthYear, 'birthYear should be unchanged');
   t.is(refUser.birthMonth, referenceUserData.birthMonth, 'birthMonth should be unchanged');
   t.is(refUser.birthDate, referenceUserData.birthDate, 'birthDay should be unchanged');
   t.is(refUser.imageURL, referenceUserData.imageURL, 'imageURL should be unchanged');
   t.is(refUser.funImageURL, referenceUserData.funImageURL, 'funImageURL should be unchanged');
+  t.is(
+    refUser.defaultedImageURL,
+    referenceUserData.imageURL,
+    'defaultedImageURL should be same as imageURL',
+  );
 });
 
 test.todo('Name should be properly transformed (trimmed and dup whitespace removed');
@@ -35,6 +41,15 @@ test.todo('Phone should be properly transformed');
 test.todo('Gender should be properly transformed');
 test.todo('ImageURL should be properly transformed');
 test.todo('funImageURL should be properly transformed');
+
+test('defaultedImageURL should always be a valid URL', async (t) => {
+  const user = new User({ ...referenceUserData, imageURL: undefined });
+  const { defaultedImageURL } = user;
+
+  const response = await axios.get(defaultedImageURL);
+  t.is(response.status, 200, 'A successful response should come back');
+  t.is(response.headers['content-type'], 'image/png', 'An image should come back');
+});
 
 test.after.always(async () => {
   await mongoose.disconnect();
