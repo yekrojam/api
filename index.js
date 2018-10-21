9dd8789ed38b7ed371c7ef79f09c163cc3948791
Makinde Adeagbo <makinde@adeagbo.com>
2018-10-21 14:21:30 -0700

    Moving all the v1 (express-restify-mongoose) into its own folder
    ...to make way for v2! No functional change here.

--- index.js
+++ index.js
@@ -4,26 +4,17 @@ const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
 const methodOverride = require('method-override');
-const restify = require('express-restify-mongoose');
 const jwt = require('express-jwt');
 
-const restifyOptions = require('./utils/restifyOptions');
 const errorHandler = require('./middleware/errorHandler');
 const notFound = require('./middleware/notFound');
 const devTokenHandler = require('./middleware/devTokenHandler');
 
+const v1Router = require('./v1');
+
 // Verify that the env has everything we need to get started. Fail fast if not
 require('./utils/envVerification').verify();
 require('./utils/connectMongoose')();
-
-const Org = require('./orgModel');
-const User = require('./userModel');
-const Membership = require('./membershipModel');
-
-const router = express.Router();
-restify.serve(router, Org, restifyOptions);
-restify.serve(router, User, restifyOptions);
-restify.serve(router, Membership, restifyOptions);
 
 const app = express();
 
@@ -36,7 +27,7 @@ app.use(
   compression(),
   devTokenHandler,
   jwt({ secret: process.env.JWT_SECRET }).unless(req => !!req.user),
-  router,
+  v1Router,
   notFound,
   errorHandler,
 );
