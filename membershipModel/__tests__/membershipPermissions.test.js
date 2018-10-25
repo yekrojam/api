a7fd6911cf1af0943e4869c19c1b237bdc251521
Makinde Adeagbo <makinde@adeagbo.com>
2018-10-24 22:14:08 -0700

    Permissions for Memberships

--- membershipModel/__tests__/membershipPermissions.test.js
+++ membershipModel/__tests__/membershipPermissions.test.js
@@ -2,4 +2,16 @@ const test = require('ava');
 
 // const Membership = require('../');
 
-test.todo('Fill in once permissions are written');
+test.todo('Logged out users cannot create memberships');
+test.todo('A member cannot create a new membership');
+test.todo('An admin can create a new membership');
+test.todo('A logged in user can add themselves if there are no other members');
+test.todo('Admins can only edit roles');
+test.todo('A user cannot edit their own membership');
+test.todo('Members can see memberships');
+test.todo('Admins can see memberships');
+test.todo('Users outside the org cannot see memberships');
+test.todo('Logged out users cannot see memberships');
+test.todo('Admins can remove memberships');
+test.todo('Members can remove their own membership');
+
--- membershipModel/membershipPermissions.js
+++ membershipModel/membershipPermissions.js
@@ -1,4 +1,50 @@
-/* eslint-disable no-unused-vars */
-module.exports = (schema) => {
+const _ = require('lodash');
+const Membership = require('./');
 
+module.exports = (schema) => {
+  const permissions = {
+    creator: {
+      create: true,
+      write: ['org', 'user', 'roles', 'createAt', 'updatedAt'],
+    },
+    admin: {
+      write: ['roles', 'updatedAt'],
+      read: ['_id', 'id', 'org', 'user', 'roles', 'createdAt'],
+      remove: true,
+    },
+    member: {
+      read: ['_id', 'id', 'org', 'user', 'roles', 'createdAt'],
+    },
+    ownMembership: {
+      remove: true,
+    },
+  };
+
+  async function getAuthLevel(payload, doc) {
+    const { userId, userMemberships } = payload;
+    const membership = _.find(userMemberships, ['org', doc.org]);
+    const levels = [];
+    const isAdmin = _.includes(membership.roles, Membership.ROLES.Admin);
+    const isMember = _.includes(membership.roles, Membership.ROLES.Member);
+    const ownMembership = doc.user === userId;
+
+    // User adding themselves to an org with no existing members (probably setting up
+    // an org). Do the expensive check to see if there are no admins of this org
+    if (
+      doc.isNew
+      && ownMembership
+      && await Membership.countDocuments({ org: doc.org }).setAuthLevel(false).exec() === 0
+    ) {
+      levels.push('creator');
+    }
+
+    if (doc.isNew && isAdmin) levels.push('creator');
+    if (isAdmin) levels.push('admin');
+    if (isMember) levels.push('member');
+    if (ownMembership) levels.push('ownMembership');
+
+    return levels;
+  }
+
+  Object.assign(schema, { permissions, getAuthLevel });
 };
