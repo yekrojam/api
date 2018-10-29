6f1236a5a43a550b5e1231f863198ee15009401d
Makinde Adeagbo <makinde@devcolor.org>
2018-10-29 01:20:59 -0700

    Streamlining Population on v2 (#50)
    Streamlining Population on v2
    
    I read more about how ResourceJS works. I was able to streamline the hooks and handlers.

--- v2/defaultOptions.js
+++ v2/defaultOptions.js
@@ -1,58 +1,64 @@
 const getQueryOpts = require('../utils/getQueryOpts');
 
+function setModelQueryHandler(req, res, next) {
+  const queryOpts = getQueryOpts(req);
+  req.modelQuery = this.model.find().setOptions(queryOpts);
+
+  next();
+}
+
+function setWriteOptionsHandler(req, res, next) {
+  req.writeOptions = getQueryOpts(req);
+  next();
+}
+
+function setPopulateOptionsHook(req, res, item, next) {
+  // This is a little strange. When passed as a hook in `index()`, the `item` is
+  // the query. When passed to `get()`, the `item` is the search object. Poor
+  // API.
+  //
+  // To reliably get the query, we read it from `req.modelQuery` since we know
+  // it should hold a reference because we set it in an earlier handler.
+  //
+  // Also, we do this logic in a hook (as opposed to a handler) because the populate
+  // options should have have been set by this point.
+  const queryOpts = getQueryOpts(req);
+  req.modelQuery.setPopulateOptions(queryOpts, ['sort', 'limit', 'skip', 'select']);
+  next();
+}
+
+function customDeleteResponseHandler(req, res, next) {
+  const { resource } = req;
+  if (resource.deleted) {
+    // DELETE worked. Transform it a little bit so client can do some magic
+    resource.status = 200;
+    resource.item = resource.item.id;
+  }
+
+  next();
+}
+
 module.exports = {
-  before: [
-    function queryAndWriteOptions(req, res, next) {
-      const queryOpts = getQueryOpts(req);
-
-      if (req.method === 'GET') {
-        queryOpts.lean = { virtuals: true };
-      }
-
-      req.modelQuery = this.model.find().setOptions(queryOpts);
-      req.writeOptions = queryOpts;
-
-      // Resource.js only supports really basic population. We manually inspect
-      // the populate query param to allow it to be a full options object
-      if (req.query.populate) {
-        let populateSetting = req.query.populate;
-        if (populateSetting[0] === '{' || populateSetting[0] === '[') {
-          populateSetting = JSON.parse(populateSetting);
-        }
-
-        req.modelQuery
-          .populate(populateSetting)
-          .setPopulateOptions(queryOpts, ['sort', 'limit', 'skip', 'select']);
-
-        // Remove `populate` from `req.query` so Resource doesn't try to handle it.
-        delete req.query.populate;
-      }
-
-      next();
-    },
-  ],
+  before: [setModelQueryHandler],
   beforeIndex: [],
-  beforePost: [],
+  beforePost: [setWriteOptionsHandler],
   beforeGet: [],
-  beforePut: [],
-  beforePatch: [],
-  beforeDelete: [],
+  beforePut: [setWriteOptionsHandler],
+  beforePatch: [setWriteOptionsHandler],
+  beforeDelete: [setWriteOptionsHandler],
   // after: [],
   afterIndex: [],
   afterPost: [],
   afterGet: [],
   afterPut: [],
   afterPatch: [],
-  afterDelete: [
-    function customDeleteResponse(req, res, next) {
-      const { resource } = req;
-      if (resource.deleted) {
-        // DELETE worked. Transform it a little bit so client can do some magic
-        resource.status = 200;
-        resource.item = resource.item.id;
-      }
-
-      next();
+  afterDelete: [customDeleteResponseHandler],
+  hooks: {
+    index: {
+      before: setPopulateOptionsHook,
     },
-  ],
+    get: {
+      before: setPopulateOptionsHook,
+    },
+  },
 };
